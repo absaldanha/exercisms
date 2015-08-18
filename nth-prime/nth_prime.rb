9efc97f9db78564479226aa07b0066800be4c54e
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2015-08-18 12:43:31 -0300

    Using lazy

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -6,7 +6,7 @@ class Prime
 
     3.step(by: 2) do |x|
       return primes.last if primes.size == n
-      primes_to_test = primes.take_while { |prime| prime <= Math.sqrt(x) }
+      primes_to_test = primes.lazy.take_while { |prime| prime <= Math.sqrt(x) }
       primes << x unless primes_to_test.any? { |prime| x % prime == 0 }
     end
   end
