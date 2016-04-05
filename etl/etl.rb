65d067d54bcc4483534dce67d3ebe3cdbba8f9c8
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2016-04-05 01:28:07 -0300

    First iteration

---
+++ etl/etl.rb
@@ -0,0 +1,7 @@
+class ETL
+  def self.transform(old_data)
+    old_data.each_with_object({}) do |(value, letters), new_data|
+      letters.each { |letter| new_data[letter.downcase] = value }
+    end
+  end
+end
